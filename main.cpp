2fbe48b74908ef9805e7ee8b007cda7dc77c397e
bogdanmagometa <bogdan.magometa@gmail.com>
2021-06-16 22:50:00 +0300

    GCD of two integers.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "utilities.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    cout << gcd(x, y) << endl;
+}
---
+++ utilities.h
@@ -0,0 +1,9 @@
+// GCD of two positive integers using Euclidian algorithm
+int gcd(int x, int y) {
+    while (y != 0) {
+        int r = x % y;
+        x = y;
+        y = r;
+    }
+    return x;
+}
